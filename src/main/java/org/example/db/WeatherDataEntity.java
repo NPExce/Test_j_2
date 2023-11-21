79eda2a12903566845e8d766bddd74ddc7a7a10a
NPExce <adrian.rostkowski01@gmail.com>
2023-11-21 17:05:59 +0100

    init commit

---
+++ src/main/java/org/example/db/WeatherDataEntity.java
@@ -0,0 +1,13 @@
+package org.example.db;
+
+import java.time.LocalDate;
+
+public class WeatherDataEntity {
+	private Long id;
+	private Long cityId;
+	
+	private LocalDate date;
+	private Long temperature;
+	private Long windSpeed;
+	private Long pressure;
+}
